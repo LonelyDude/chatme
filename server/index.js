6747b772b21845d8215b34a5a7b0f63b3751e16d
Bruno Soares <brunofas12@gmail.com>
2024-10-01 22:48:32 +0800

    make a realtime server

--- server/index.js
+++ server/index.js
@@ -1,56 +1,49 @@
 //
-const express = require('express');
-const cors = require('cors');
 const http = require('http');
-const PORT = 6000 || 6010;
-
-const hostname = `http://www.localhost:${PORT}`;
-
-const app = express();
-
-// app.use(
-//   cors({
-//     origin: hostname,
-//     methods: ['GET, POST'],
-//     allowedHeaders: ['my-custom-header'],
-//     credentials: true,
-//   })
-// );
-
-//Sending messages to cliente side
-app.get('/', (req, res) => {
-  res.send('HELLO BACKEND');
-  console.log('HI THERE');
-});
-
-// const { Server } = require('socket.io');
-
-// const server = http.createServer(app);
-
+const express = require('express')
+const cors = require('cors')
+const PORT = 8080 || 8000
+const hostname = `http://www.localhost:${PORT}`
+// import http from 'http'
 //
+
+//Connect app with express
+const app = express()
+//Cors
+app.use(cors({
+  origin: hostname,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["my-custom-header"],
+  credentials: true
+}))
+//Make a server
+const server = http.createServer(app)
+//Import sockect.io server
 const { Server } = require('socket.io');
 
-const server = http.createServer(app);
-const io = new Server(server);
-//
+const io = new Server(server,)
+
 io.on('connection', (socket) => {
   console.log('A user connected');
-  //Handle messages
+  
+  // Handle chat messages
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    //
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
   });
-  //Disconnection
+//Disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
-
-  //
-  io.engine('connection_error', (err) => {
-    console.log(err.message);
-  });
 });
 
-//listening
-app.listen(PORT, () => {
-  console.log(`Listening to ${hostname}`);
+io.engine.on("connection_error", (err) => {
+  // console.log(err.req);      // the request object
+  // console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  // console.log(err.context);  // some additional error context
+});
+
+server.listen(PORT, () => {
+  console.log(`WebSocket server listening on ${hostname}`);
 });
